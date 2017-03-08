c7c14424cf944b922d935d5a6c2aea644378c6f2
Ryan Gonzalez <rymg19@gmail.com>
2017-03-08 11:34:46 -0600

    PyPy has been working for a while now

--- travis.sh
+++ travis.sh
@@ -2,24 +2,8 @@
 
 set -e
 
-pypy_url=http://buildbot.pypy.org/nightly/trunk/pypy-c-jit-latest-linux64.tar.bz2
-pip_url=https://bootstrap.pypa.io/get-pip.py
-python=python
-pip=pip
-
-if $python --version 2>&1 | grep PyPy; then
-    echo "Fetching PyPy..."
-    curl $pypy_url -o pypy.tbz2
-    echo "Extracting PyPy..."
-    tar xf pypy.tbz2
-    python=pypy-*/bin/pypy
-    echo "Downloading pip..."
-    curl $pip_url | $python
-    pip=pypy-*/bin/pip
-fi
-
 echo "Installing dependencies..."
-$pip install -r requirements.txt
+pip install -r requirements.txt
 
 echo "Running tests..."
-$python ./hytest
+python ./hytest
